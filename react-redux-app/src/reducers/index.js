8ae6da5d400a486adb2ab0dd0730371d2c1084a6
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-11-07 14:49:57 -0800

    Initial Commit: file structure &  reducer created

---
+++ react-redux-app/src/reducers/index.js
@@ -0,0 +1,13 @@
+export const initialState = {
+    weatherData: {},
+    isFetching: false,
+    error: null
+};
+
+function reducer(state = initialState, action){
+    switch(action.type){
+    default:
+        return state}
+};
+
+export default reducer;
